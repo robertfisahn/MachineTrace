58b46db29c3b8cd1088f2916c7ded6ac076ac8ce
Robert Fisahn <robertfisahn@gmail.com>
2023-10-31 01:31:17 +0100

    Add FluentValidation

--- MachineTrace.Application/Extensions/ServiceCollectionExtension.cs
+++ MachineTrace.Application/Extensions/ServiceCollectionExtension.cs
@@ -1,5 +1,8 @@
-﻿using MachineTrace.Application.Mappings;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using MachineTrace.Application.Mappings;
 using MachineTrace.Application.Services;
+using MachineTrace.Application.Validators;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MachineTrace.Application.Extensions
@@ -10,6 +13,10 @@ namespace MachineTrace.Application.Extensions
         {
             serviceCollection.AddScoped<ICategoryService, CategoryService>();
             serviceCollection.AddAutoMapper(typeof(MachineTraceMappingProfile));
+            serviceCollection.AddValidatorsFromAssemblyContaining<CategoryUpsertDtoValidator>()
+                .AddFluentValidationAutoValidation()
+                .AddFluentValidationClientsideAdapters();
+
         }
     }
 }
---
+++ MachineTrace.Application/Validators/CategoryUpsertDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MachineTrace.Application.Dto.Category;
+using MachineTrace.Domain.Intefaces;
+
+namespace MachineTrace.Application.Validators
+{
+    public class CategoryUpsertDtoValidator : AbstractValidator<CategoryDto>
+    {
+        public CategoryUpsertDtoValidator(ICategoryRepository repository)
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .MaximumLength(25).WithMessage("Name is too long. Maximum numbers of characters is 25.")
+                .Custom((value, context) =>
+                {
+                    var existingName = repository.GetByName(value).Result;
+                    if(existingName != null)
+                    {
+                        context.AddFailure($"{value} already exist");
+                    }
+                });
+        }
+    }
+}
